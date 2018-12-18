061b20320dc1475efdfc0813a8f59f351ae39312
dhs_ls <lin827261413@163.com>
2018-12-18 16:14:33 +0800

    添加代码控制开关

--- app/src/main/java/com/dahuaishu365/switchview/MainActivity.java
+++ app/src/main/java/com/dahuaishu365/switchview/MainActivity.java
@@ -2,12 +2,39 @@ package com.dahuaishu365.switchview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private SwitchView mSwitchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSwitchView = (SwitchView) findViewById(R.id.switch_view);
+        findViewById(R.id.button).setOnClickListener(this);
+        findViewById(R.id.button2).setOnClickListener(this);
+        findViewById(R.id.button3).setOnClickListener(this);
+        findViewById(R.id.button4).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button:
+                mSwitchView.turnOn();
+                break;
+            case R.id.button2:
+                mSwitchView.turnOffWithAnimation();
+                break;
+            case R.id.button3:
+                mSwitchView.turnOnWithAnimation();
+                break;
+            case R.id.button4:
+                mSwitchView.turnOff();
+                break;
+        }
+
     }
 }
--- app/src/main/java/com/dahuaishu365/switchview/SwitchView.java
+++ app/src/main/java/com/dahuaishu365/switchview/SwitchView.java
@@ -50,10 +50,10 @@ public class SwitchView extends View {
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.MySwitchView);
         mSw_circle_width = (int) typedArray.getDimension(R.styleable.MySwitchView_sw_circle_width, -1);
         mSw_circle_margin = (int) typedArray.getDimension(R.styleable.MySwitchView_sw_circle_margin, 0);
-        mAnimation_duration =  typedArray.getInt(R.styleable.MySwitchView_animation_duration, 500);
-        mOn_color =  typedArray.getColor(R.styleable.MySwitchView_on_color,Color.parseColor("#EA3385"));
+        mAnimation_duration = typedArray.getInt(R.styleable.MySwitchView_animation_duration, 500);
+        mOn_color = typedArray.getColor(R.styleable.MySwitchView_on_color, Color.parseColor("#EA3385"));
         mBg_color = typedArray.getColor(R.styleable.MySwitchView_bg_color, Color.parseColor("#f5f5f5"));
-        mOff_color = typedArray.getColor(R.styleable.MySwitchView_on_color,Color.parseColor("#D8D8D8"));
+        mOff_color = typedArray.getColor(R.styleable.MySwitchView_on_color, Color.parseColor("#D8D8D8"));
         mCircle_color = mOff_color;
         typedArray.recycle();
         init();
@@ -65,8 +65,8 @@ public class SwitchView extends View {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         mMeasuredWidth = getMeasuredWidth();
         mMeasuredHeight = getMeasuredHeight();
-        if(mSw_circle_width ==-1){
-            mSw_circle_width = mMeasuredWidth/2;
+        if (mSw_circle_width == -1) {
+            mSw_circle_width = mMeasuredWidth / 2;
         }
         mMoveDistance = mMeasuredWidth - mSw_circle_width;
     }
@@ -75,6 +75,45 @@ public class SwitchView extends View {
 
     }
 
+    /**
+     * 转到on
+     */
+    public void turnOn() {
+        mCircle_left = mMoveDistance;
+        moveRight = false;
+        mCircle_color = mOn_color;
+        if (l != null)
+            l.turnOn();
+        invalidate();
+
+    }
+
+    /**
+     * 转到off
+     */
+    public void turnOff() {
+        mCircle_left = 0;
+        moveRight = true;
+        mCircle_color = mOff_color;
+        if (l != null)
+            l.turnOff();
+        invalidate();
+
+    }
+    /**
+     * 转到on 带动画
+     */
+    public void turnOnWithAnimation() {
+        moveRight = false;
+        turnWithAnimation();
+    }
+    /**
+     * 转到off 带动画
+     */
+    public void turnOffWithAnimation() {
+        moveRight = true;
+        turnWithAnimation();
+    }
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
@@ -84,19 +123,20 @@ public class SwitchView extends View {
         Paint paint1 = new Paint();
         paint1.setColor(mCircle_color);
         paint1.setAntiAlias(true);
-        canvas.drawRoundRect(mCircle_left + mSw_circle_margin, mSw_circle_margin, mSw_circle_width + mCircle_left -mSw_circle_margin
+        canvas.drawRoundRect(mCircle_left + mSw_circle_margin, mSw_circle_margin, mSw_circle_width + mCircle_left - mSw_circle_margin
                 , mMeasuredHeight - mSw_circle_margin, mMeasuredHeight / 2, mMeasuredHeight / 2, paint1);
     }
 
-    public interface OnTurnListener{
+    public interface OnTurnListener {
 
         void turnOff();
 
         void turnOn();
     }
+
     private OnTurnListener l;
 
-    public void setOnTurnListener(OnTurnListener l){
+    public void setOnTurnListener(OnTurnListener l) {
         this.l = l;
     }
 
@@ -122,55 +162,59 @@ public class SwitchView extends View {
                     return true;
                 }
                 if (isClick) {
-                    ValueAnimator animator = ValueAnimator.ofInt(0, mMoveDistance);
-                    animator.setDuration(mAnimation_duration);
-                    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                        @Override
-                        public void onAnimationUpdate(ValueAnimator animation) {
-                            mCircle_left = (int) animation.getAnimatedValue();
-                            if (!moveRight) {
-                                mCircle_left = mMoveDistance - mCircle_left;
-                            }
-                            invalidate();
-                        }
-                    });
-                    animator.addListener(new Animator.AnimatorListener() {
-                        @Override
-                        public void onAnimationStart(Animator animation) {
-                            isPlaying = true;
-                        }
-
-                        @Override
-                        public void onAnimationEnd(Animator animation) {
-                            isPlaying = false;
-                            if (mCircle_left == 0) {
-                                moveRight = true;
-                                mCircle_color = mOff_color;
-                                if(l !=null)
-                                l.turnOff();
-                            } else {
-                                moveRight = false;
-                                mCircle_color = mOn_color;
-                                if(l !=null)
-                                l.turnOn();
-                            }
-                        }
-
-                        @Override
-                        public void onAnimationCancel(Animator animation) {
-                            isPlaying = false;
-                        }
-
-                        @Override
-                        public void onAnimationRepeat(Animator animation) {
-
-                        }
-                    });
-                    animator.start();
+                    turnWithAnimation();
                     return true;
                 }
                 break;
         }
         return super.onTouchEvent(event);
+    }
+
+    private void turnWithAnimation() {
+        ValueAnimator animator = ValueAnimator.ofInt(0, mMoveDistance);
+        animator.setDuration(mAnimation_duration);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                mCircle_left = (int) animation.getAnimatedValue();
+                if (!moveRight) {
+                    mCircle_left = mMoveDistance - mCircle_left;
+                }
+                invalidate();
+            }
+        });
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                isPlaying = true;
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                isPlaying = false;
+                if (mCircle_left == 0) {
+                    moveRight = true;
+                    mCircle_color = mOff_color;
+                    if (l != null)
+                        l.turnOff();
+                } else {
+                    moveRight = false;
+                    mCircle_color = mOn_color;
+                    if (l != null)
+                        l.turnOn();
+                }
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                isPlaying = false;
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        animator.start();
     }
 }
